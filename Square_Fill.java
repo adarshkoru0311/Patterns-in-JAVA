48ab749678fe7f34c77c1bbd3f261da723004c10
adarsh_koru <104086266+adarshkoru0311@users.noreply.github.com>
2024-03-27 00:14:50 +0530

    Add files via upload
    This code will print the following Square Fill pattern 
    
            * * * * * 
    	* * * * * 
    	* * * * * 
    	* * * * * 
    	* * * * *

---
+++ Square_Fill.java
@@ -0,0 +1,26 @@
+/*
+  Output :
+  	
+    * * * * * 
+	* * * * * 
+	* * * * * 
+	* * * * * 
+	* * * * * 
+	
+ */
+
+public class Square_Fill {
+	public static void main(String[] args) {
+
+		int r = 5, c = 5;
+		for (int i = 1; i <= r; i++) {
+
+			for (int j = 1; j <= c; j++)
+				System.out.print("* ");
+
+			System.out.print("\n");
+		}
+
+	}
+
+}
