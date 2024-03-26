7af175485be847693609abf865a6aa2913f36bfb
adarsh_koru <104086266+adarshkoru0311@users.noreply.github.com>
2024-03-27 00:23:25 +0530

    Add files via upload

---
+++ Triangle_Star_Pyramid.java
@@ -0,0 +1,24 @@
+/*
+  Output :
+
+     *
+    * *
+   * * *
+  * * * *
+ * * * * *
+ 
+ */
+public class Triangle_Star_Pyramid {
+    public static void main(String[] args) {
+        int r = 5, c = 5;
+
+        for (int i = 1; i <= r; i++) {
+            for (int k = 1; k <= c - i; k++)
+                System.out.print(" ");
+
+            for (int j = 1; j <= i; j++)
+                System.out.print(" *");
+            System.out.println();
+        }
+    }
+}
