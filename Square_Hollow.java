e686a0988bce71e0309bb24f8760ab545c8b9379
adarsh_koru <104086266+adarshkoru0311@users.noreply.github.com>
2024-03-27 00:16:55 +0530

    Add files via upload

---
+++ Square_Hollow.java
@@ -0,0 +1,26 @@
+/*
+Output :
+ 
+ * * * * *
+ *       *
+ *       *
+ *       *
+ * * * * *
+ 
+ */
+public class Square_Hollow {
+    public static void main(String[] args) {
+        int r = 5, c = 5;
+        for (int i = 1; i <= r; i++) {
+
+            for (int j = 1; j <= c; j++) {
+
+                if (i == 1 || j == 1 || i == r || j == c)
+                    System.out.print(" *");
+                else
+                    System.out.print("  ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
